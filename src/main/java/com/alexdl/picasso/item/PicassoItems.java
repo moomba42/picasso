239430afbdfb6badef1f2d861102210acf71de89
Aleksander Długosz <olekdlugi@gmail.com>
2025-03-29 16:54:26 +0100

    Define item stack sizes

--- src/main/java/com/alexdl/picasso/item/PicassoItems.java
+++ src/main/java/com/alexdl/picasso/item/PicassoItems.java
@@ -10,16 +10,17 @@ public class PicassoItems {
     private static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Picasso.MOD_ID);
 
     public static final DeferredItem<Item> HORSEHAIR = ITEMS.register("horsehair",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(64)));
 
     public static final DeferredItem<Item> PAINTBRUSH_SMALL = ITEMS.register("paintbrush_small",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
 
     public static final DeferredItem<Item> PAINTBRUSH_MEDIUM = ITEMS.register("paintbrush_medium",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
 
     public static final DeferredItem<Item> PAINTBRUSH_BIG = ITEMS.register("paintbrush_big",
             () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
